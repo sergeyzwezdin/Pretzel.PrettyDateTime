15b069311cf1de30982c4ff6f16cf24da964db82
Sergey Zwezdin <sergey@zwezdin.com>
2016-07-02 07:59:24 +0500

    Initial version

---
+++ Pretzel.PrettyDateTime/PrettyDateFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pretzel.Logic.Extensibility;
+
+namespace Pretzel.PrettyDateTime
+{
+    public class PrettyDateFilter : IFilter
+    {
+        public string Name
+        {
+            get { return "PrettyDate"; }
+        }
+
+        public static string PrettyDate(DateTime input)
+        {
+            return input.ToString("d MMMM, yyyy");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Pretzel.PrettyDateTime
+
+Allows you to format date as `.ToString("d MMMM, yyyy")`.
+
+This is a plugin for the static site generation tool [Pretzel](https://github.com/Code52/pretzel).
+
+### Usage
+
+```html
+<p>{{ post.date | pretty_date }}</p>
+```
+
+### Installation
+
+Download the [latest release](https://github.com/sergeyzwezdin/Pretzel.PrettyDateTime/releases/latest) and copy `Pretzel.PrettyDateTime.dll` to the `_plugins` folder at the root of your site folder.
